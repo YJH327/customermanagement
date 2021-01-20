29de1567571c5c5fe7ac4819843795d026716060
YJH327 <didwnsgh327@gmail.com>
2021-01-20 23:20:19 +0900

    Customer Table (Front_End)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Customertablehead, Customertablebody } from './componenets/Customer'
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import TableBody from '@material-ui/core/TableBody';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '양준호',
+  'birthday': '980327',
+  'gender': '남자',
+  'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '양경선',
+    'birthday': '630903',
+    'gender': '남자',
+    'job': '한의사'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '양진솔',
+    'birthday': '950301',
+    'gender': '여자',
+    'job': '취준생'
+  }
+]
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customer table">
+          <Customertablehead />
+          <TableBody>
+          {customers.map(c => {
+            return <Customertablebody key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
---
+++ client/src/componenets/Customer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+
+export function Customertablehead(props) {
+    
+    return(
+        <TableHead>
+            <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                 <TableCell>직업</TableCell>
+            </TableRow>
+        </TableHead>      
+    )
+}
+
+export function Customertablebody(props) {
+
+    return(
+        <TableRow>
+            <TableCell>{props.id}</TableCell>
+            <TableCell><img src={props.image} alt="profile"/></TableCell>
+            <TableCell>{props.name}</TableCell>
+            <TableCell>{props.birthday}</TableCell>
+            <TableCell>{props.gender}</TableCell>
+            <TableCell>{props.job}</TableCell>
+        </TableRow>
+    )
+}
